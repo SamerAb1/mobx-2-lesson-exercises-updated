8c5c83ee080cdee4b2fa2829b4de61ab2156eaab
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-08-21 13:05:53 +0300

    solving exercises 1-3

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react'
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
 
 class Reservation extends Component {
-    render() {
-        return (
-            <div></div>
-            //render the reservation data here
-            //make sure you store the ID somewhere so you can find the reservation
-            //use the class "conditional" to conditionally render completed reservations
-            //You should hav ea complete reservation button to complete the reservation
-        )
-    }
+  render() {
+    const { res, RestaurantStore } = this.props;
+
+    return (
+      <div className={`reservation ${res.completed ? "conditional" : ""}`}>
+        <div className="reservation__row">
+          <span className="reservation__name">{res.name}</span>
+          <span className="reservation__people"> — {res.numPeople} people</span>
+
+          {res.seated && !res.completed && (
+            <span className="reservation__tag">SEATED</span>
+          )}
+          {res.completed && <span className="reservation__tag">DONE</span>}
+        </div>
+
+        <div className="reservation__actions">
+          {!res.seated && (
+            <button onClick={() => RestaurantStore.seatRes(res.id)}>
+              Seat reservation
+            </button>
+          )}
+          {!res.completed && (
+            <button onClick={() => RestaurantStore.completeRes(res.id)}>
+              Complete reservation
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-//inject your store here
-export default Reservation
+export default inject("RestaurantStore")(observer(Reservation));
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,23 +1,50 @@
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react'
-import ResInput from './ResInput';
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+import ResInput from "./ResInput";
+import Reservation from "./Reservation";
 
-class Restaurant extends Component{
-    render () {
-        return (
-            <div>
-                <span>You have {this.props.RestaurantStore.openTables} open tables</span>
-                {/* Add in # of people in restaurant */}
-                {/* Add in # of completed tables with id "completedTables*/}
-                <ResInput/>
-                <button id="addRes">Add Reservation</button>
-                {/* Make the Add Reservation button work */}
-                <div className = "reservations">
-                {/* Map reservation data to Reservation components here */}
-                </div>
-            </div>
-        )
-    }
+class Restaurant extends Component {
+  addReservation = () => {
+    const { GeneralStore, RestaurantStore } = this.props;
+    const { name, numPeople } = GeneralStore;
+    RestaurantStore.addRes(name, Number(numPeople)); // use both stores
+  };
+
+  render() {
+    const { RestaurantStore } = this.props;
+    return (
+      <div>
+        <span>
+          You have {this.props.RestaurantStore.openTables} open tables
+        </span>
+        {
+          /* Add in # of people in restaurant */
+          <div>
+            You have {this.props.RestaurantStore.restPopulation} people in the
+            resturant
+          </div>
+        }
+        {
+          /* Add in # of completed tables with id "completedTables*/
+          <div id="completedTables">
+            You have {this.props.RestaurantStore.completedTables} completed
+            tables in the resturant
+          </div>
+        }
+        <ResInput />
+        <button id="addRes" onClick={this.addReservation}>
+          Add Reservation
+        </button>
+        {/* Make the Add Reservation button work */}
+        <div className="reservations">
+          {/* Map reservation data to Reservation components here */}
+          {RestaurantStore.reservations.map((r) => (
+            <Reservation key={r.id} res={r} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default inject("GeneralStore", "RestaurantStore")(observer(Restaurant))
+export default inject("GeneralStore", "RestaurantStore")(observer(Restaurant));
